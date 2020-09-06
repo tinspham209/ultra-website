18824f3d6c564fdc1ce017786c045b5126c3b836
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-06 11:14:33 +0700

    NavBar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { Button } from "../../globalStyles";
+import {
+	MobileIcon,
+	Nav,
+	NavbarContainer,
+	NavIcon,
+	NavLogo,
+	NavMenu,
+	NavItem,
+	NavLinks,
+	NavItemBtn,
+	NavBtnLink,
+} from "./Navbav.elements";
+
+const Navbar = () => {
+	const [click, setClick] = useState(false);
+	const [button, setButton] = useState(true);
+
+	const handleClick = () => {
+		setClick(!click);
+	};
+
+	const showButton = () => {
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+
+	useEffect(() => {
+		showButton();
+	}, []);
+
+	window.addEventListener("resize", showButton);
+
+	return (
+		<>
+			<IconContext.Provider value={{ color: "#fff" }}>
+				<Nav>
+					<NavbarContainer>
+						<NavLogo to="/">
+							<NavIcon />
+							Ultra
+						</NavLogo>
+						<MobileIcon onClick={handleClick}>
+							{click ? <FaTimes /> : <FaBars />}
+						</MobileIcon>
+						<NavMenu onClick={handleClick} click={click}>
+							<NavItem>
+								<NavLinks to="/">Home</NavLinks>
+							</NavItem>
+							<NavItem>
+								<NavLinks to="/services">Services</NavLinks>
+							</NavItem>
+							<NavItem>
+								<NavLinks to="/products">Products</NavLinks>
+							</NavItem>
+							<NavItemBtn>
+								{button ? (
+									<NavBtnLink to="/sign-up">
+										<Button primary>SIGN UP</Button>
+									</NavBtnLink>
+								) : (
+									<NavBtnLink to="/sign-up">
+										<Button fontBig primary>
+											SIGN UP
+										</Button>
+									</NavBtnLink>
+								)}
+							</NavItemBtn>
+						</NavMenu>
+					</NavbarContainer>
+				</Nav>
+			</IconContext.Provider>
+		</>
+	);
+};
+
+export default Navbar;
