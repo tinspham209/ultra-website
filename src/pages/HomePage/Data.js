0fdc479b2b760bf547ec2117b11fca82ff519352
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-06 14:30:23 +0700

    Setup Data for section

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+	lightBg: false,
+	primary: true,
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	imgStart: "",
+	topLine: "Margeting Agency",
+	headline: "Lead Generation Specialist for Online Businesses",
+	description:
+		"We help business owners increase their revenue. Our team if unique specialist can help you archive your business goals.",
+	buttonLabel: "Get Started",
+	img: require("../../assets/images/svg-1.svg"),
+	alt: "Image",
+	start: "",
+};
+
+export const homeObjTwo = {
+	lightBg: true,
+	primary: false,
+	lightTopLine: false,
+	lightText: false,
+	lightTextDesc: false,
+	imgStart: "start",
+	topLine: "Margeting Agency",
+	headline: "Lead Generation Specialist for Online Businesses",
+	description:
+		"We help business owners increase their revenue. Our team if unique specialist can help you archive your business goals.",
+	buttonLabel: "Get Started",
+	img: require("../../assets/images/profile.jpg"),
+	alt: "Image",
+	start: "true",
+};
+
+export const homeObjThree = {
+	lightBg: false,
+	primary: true,
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	imgStart: "",
+	topLine: "Margeting Agency",
+	headline: "Lead Generation Specialist for Online Businesses",
+	description:
+		"We help business owners increase their revenue. Our team if unique specialist can help you archive your business goals.",
+	buttonLabel: "Get Started",
+	img: require("../../assets/images/svg-2.svg"),
+	alt: "Image",
+	start: "",
+};
+
+export const homeObjFour = {
+	lightBg: false,
+	primary: true,
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	imgStart: "start",
+	topLine: "Margeting Agency",
+	headline: "Lead Generation Specialist for Online Businesses",
+	description:
+		"We help business owners increase their revenue. Our team if unique specialist can help you archive your business goals.",
+	buttonLabel: "Get Started",
+	img: require("../../assets/images/svg-3.svg"),
+	alt: "Image",
+	start: "true",
+};
