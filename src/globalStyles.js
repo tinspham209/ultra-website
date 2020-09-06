55c23d49880396239561b7b9bd5ec358f31a8d37
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-06 12:46:33 +0700

    change color btn to fff

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -31,7 +31,7 @@ export const Button = styled.button`
 	background: ${({ primary }) => (primary ? "#4B59F7" : "#0467FB")};
 	white-space: nowrap;
 	padding: ${({ big }) => (big ? "12px 64px" : "10px 20px")};
-	color: #000;
+	color: #fff;
 	font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
 	outline: none;
 	cursor: pointer;
