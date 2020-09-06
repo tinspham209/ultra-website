a73a2fa6f968e30e18cbcd266cd67caf4d2f97ef
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-06 14:19:36 +0700

    Spinner UI

---
+++ src/components/UI/Spinner/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+const Spinner = () => {
+	return (
+		<div class="blocks">
+			<div class="block orange"></div>
+			<div class="block blue"></div>
+		</div>
+	);
+};
+
+export default Spinner;
